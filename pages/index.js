050254ccde2228d98bab2c717dd9d34de44c5deb
Daniel Martins <daniellmarttins@hotmail.com>
2018-11-10 10:49:55 -0300

    setup styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default () => <h1>Hello World!</h1>;
+import React from "react";
+import styled from "styled-components";
+
+export default () => <Title>Hello World!</Title>;
+
+const Title = styled.h1`
+  color: red;
+`;
